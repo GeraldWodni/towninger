f90bbb324025343558f56d86ead7fa729988de81
Gerald Wodni <gerald.wodni@gmail.com>
2025-02-09 03:52:30 +0100

    tile switcher added

--- main.c
+++ main.c
@@ -91,10 +91,13 @@ void main() {
     SHOW_SPRITES;
     DISPLAY_ON;
 
+    UINT8 my_tile = TILE_STARFIELD;
+    UINT8 lock_button = 0;
+
     /* background */
     set_bkg_palette( 0, 8, spritePalette );
     set_bkg_data( 0, NUMBER_OF_TILES, Tiles );
-    fill_bkg_rect( 0, 0, BW, BH, TILE_STARFIELD );
+    fill_bkg_rect( 0, 0, BW, BH, my_tile );
 
     set_tile_xy(1,2,3);
 
@@ -103,8 +106,23 @@ void main() {
     drawText( 5, 7, "PRESS START" );
 
 	while (1) {
-		if (joypad() && J_A)
-			break;
+		UINT8 buttons = joypad();
+        if( buttons == 0 && lock_button )
+            lock_button = 0;
+
+        if( !lock_button )
+            switch( buttons ) {
+                case J_LEFT:
+                    if( my_tile > 0 )
+                        fill_bkg_rect( 0, 0, BW, BH, --my_tile );
+                    lock_button = 1;
+                break;
+                case J_RIGHT:
+                    if( my_tile < NUMBER_OF_TILES-1 )
+                        fill_bkg_rect( 0, 0, BW, BH, ++my_tile );
+                    lock_button = 1;
+                break;
+            }
 	}
 
 	DISPLAY_OFF;
